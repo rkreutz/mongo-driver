99a2f07bc61040a3437be605833424391b975fcd
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2018-07-10 23:52:35 -0300

    Fixing Package dependencies

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "MongoDriver",
     dependencies: [
         .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
-        .Package(url: "https://github.com/rkreutz/MongoKitten.git", majorVersion: 4, minor: 1),
+        .Package(url: "https://github.com/rkreutz/MongoKitten.git", majorVersion: 4, minor: 0),
     ]
 )
